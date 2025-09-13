dab316d7968a831b1346393932b158d3d3541aeb
AlexandreGastinel18 <gastinel.alex@gmail.com>
2025-09-13 18:27:05 +0200

    outil qui renvoie des infos sur un artiste

--- src/my_server.py
+++ src/my_server.py
@@ -2,9 +2,17 @@ from fastmcp import FastMCP
 
 mcp = FastMCP("My MCP Server")
 
+
+
+
 @mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+def get_artist_info(artist_name: str) -> dict:
+    from artist import artist
+    from get_id import search_id
+    artist_id = search_id(artist_name, type='artist')
+    if artist_id:
+        info = artist(artist_id).get_info()
+        return info
 
     
 if __name__ == "__main__":
