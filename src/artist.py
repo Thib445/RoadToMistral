dac235d27cad4f3c1a04dc508a9c33c93d3bc3b5
AlexandreGastinel18 <gastinel.alex@gmail.com>
2025-09-13 20:00:25 +0200

    commit d'artiste

--- src/artist.py
+++ src/artist.py
@@ -1,14 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-class artist():
+class Artist:
 
     def __init__(self, id, sp=None):
         self.id = id
         self.sp = sp or spotipy.Spotify(auth_manager=SpotifyClientCredentials())
         self.info = self.sp.artist(self.id)
         self.name = self.info['name']
-        self.followers = self.info["Followers"]["total"]
+        self.followers = self.info["followers"]["total"]
         self.genres = self.info['genres']
         self.popularity = self.info['popularity']
         self.type = self.info['type']
@@ -20,6 +20,7 @@ class artist():
             'name': self.name,
             'genres': self.genres,
             'followers': self.followers,
+            'popularity': self.popularity,
         }
     
     def get_name(self):
@@ -39,4 +40,11 @@ class artist():
     
     def is_artiste(self):
         return self.artiste == 'artist'
+    
+    def get_similar_artists(self):
+        results = self.sp.artist_related_artists(self.id)
+        similar_artists = []
+        for artist in results['artists']:
+            similar_artists.append(artist['name'])
+        return similar_artists
     
